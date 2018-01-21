bb99b990350b301b5df5440c0ac0275099f21e16
AlexSa <alexander.cherba@gmail.com>
2018-01-21 23:04:31 +0200

    new step

--- web-app/src/main/webapp/js/create_zoo.js
+++ web-app/src/main/webapp/js/create_zoo.js
@@ -1,6 +1,14 @@
 /**
  * Created by cube on 28.12.2017.
  */
+var doc = document;
+var elem = doc.createElement("p");
+var content = doc.createTextNode("Existing Zoos:");
+elem.appendChild(content);
+
+var reP = doc.getElementById("aboutZoos");
+reP.appendChild(content);
+
 function createZoo() {
     $.ajax({
         type: "POST",
