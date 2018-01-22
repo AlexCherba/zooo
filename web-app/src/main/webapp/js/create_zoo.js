613b72456e9f615ab2cfc1b774098bc5f2049f0a
AlexSa <alexander.cherba@gmail.com>
2018-01-22 12:30:12 +0200

    make html and js for create new zoo

--- web-app/src/main/webapp/js/create_zoo.js
+++ web-app/src/main/webapp/js/create_zoo.js
@@ -6,13 +6,32 @@ var elem = doc.createElement("p");
 var content = doc.createTextNode("Existing Zoos:");
 elem.appendChild(content);
 
-var reP = doc.getElementById("aboutZoos");
-reP.appendChild(content);
+//var reP = doc.getElementById("aboutZoos");
+//reP.appendChild(content);
 
-function createZoo() {
+var tbZoos = doc.getElementById("tb_zoo");
+
+//tbZoos.innerHTML("tr");
+
+function addNewZoo() {
+    var nameZoo = doc.getElementById("nameZoo").value;
+    var locationZoo = doc.getElementById("locationZoo").value;
+    var table = doc.getElementById("tb_zoo");
+    // or var newRow = table.insertRow();
+    var newRow = table.insertRow(table.rows.length);
+    var cell0 = newRow.insertCell(0);
+    var cell1 = newRow.insertCell(1);
+    var cell2 = newRow.insertCell(2);
+
+    cell0.innerHTML = 0;
+    cell1.innerHTML = nameZoo;
+    cell2.innerHTML = locationZoo;
+
+    /*
     $.ajax({
         type: "POST",
         url: "/createzoo",
         dataType: "json",
     });
+*/
 }
